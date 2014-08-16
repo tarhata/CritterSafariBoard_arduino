b8abae60aeb87e57cae7a2dc0ba308347571e379
tarhata <tguiamel@u.washington.edu>
2014-08-16 03:49:25 -0700

    Update Dock.ino
    Forever sends "add object ________", "remove object _______" or "set bg _______" commands to serial port 9600

--- Dock.ino
+++ Dock.ino
@@ -5,30 +5,94 @@
  * MHCID Capstone 2014
  *
  **/
+ 
 
-// Object and Setting Pieces
-#define SETTING A0
-#define OBJECT1 A1
-#define OBJECT2 A2
-#define OBJECT3 A3
-#define OBJECT4 A4
+// Object and Background Pieces
+#define OBJECT1 A0
+#define OBJECT2 A1
+#define OBJECT3 A2
+#define OBJECT4 A3
+#define BACKGROUND A4
+
+int values[] = {
+  1023, 1023, 1023, 1023, 1023};  // {object, object, object, object, background}
+
+int prevValues [] = {
+  1023, 1023, 1023, 1023, 1023}; // {object, object, object, object, background}
+
+const int range = 5; // range that accounts for variance in analog readings
+
+String type = "";
 
 void setup() {  
   Serial.begin(57600);
 }
 
+// On startup, the serial port will print "remove object" until an object is initially added
+// After initial object detection, it will continuosly print the last command called  
 void loop() {
-  for (int pin=0; pin < 6 ; pin++)
+  for (int pin=0; pin < 5; pin++)
   {
-    int reading = analogRead(pin);
-    Serial.print(reading);
-    Serial.print(" ");
-    delay(500);  
-  }
-  Serial.println();
+    int reading = analogRead(pin); 
+    //    Serial.println(reading);
+
+    int value = values[pin];
+    int prevValue = prevValues[pin];
+
+    int minReading = reading - range;
+    int maxReading = reading + range;
+
+    if (value > maxReading || value < minReading)
+    {
+      prevValues[pin] = values[pin]; // save current values
+      values[pin] = reading;        // update currently read value
+    }
+    else if (value < 1000 && pin < 4) 
+    {
+      Serial.print("add object ");
+      type = checkType(value, type);
+      Serial.println(type);    
+    } 
+    else if (value > 1000 && pin < 4) 
+    {
+      Serial.print("remove object ");
+      type = checkType(prevValue, type);
+      Serial.println(type);   
+    }
+    else if (value < 1000 && pin == 4)
+    {
+      Serial.print("set bg ");
+      type = checkType(value, type);
+      Serial.println(type);
+    } 
+  } 
+  delay(500);
+  Serial.println("--------------------");
 }
 
-
-
+String checkType(int value, String type) {
+  if (value > 15 && value < 25) {
+    type = "fox";  
+  } 
+  else if(value > 25 && value < 35) { 
+    type = "pig"; 
+  } 
+  else if (value > 85 && value < 98) {
+    type = "monkey";
+  } 
+  else if (value > 505 && value < 515) {
+    type = "alien";
+  }
+  if (value > 175 && value < 185) {
+    type = "forest";  
+  } 
+  else if (value > 5 && value < 15) {
+    type = "space";
+  } 
+  else if (value > 1000)
+  { type = "";
+  }
+  return type;
+}
 
 
