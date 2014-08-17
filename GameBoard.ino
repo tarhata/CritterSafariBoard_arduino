bd6c82abfdffa4c8a33447eb2ec85c2abb569b2a
tarhata <tguiamel@u.washington.edu>
2014-08-17 09:23:41 -0700

    Update GameBoard.ino
    Changed to output block data only once

--- GameBoard.ino
+++ GameBoard.ino
@@ -14,7 +14,6 @@ boolean charPresent = false;
 const int buttonPin = 5;     // the number of the pushbutton pin
 const int statusLED =  13;  // the number of the LED pin
 
-int buttonState = 0;     // variable for reading the pushbutton status
 const int charPin = A4; // character pin
 const int muxPin = A5; // analog multiplexer input pin
 
@@ -53,6 +52,17 @@ int stepsValues[] = {
 int functionValues[] = {
   0, 0, 0, 0};    
 
+int charVal = 1023;  
+int range = 5;
+String type = "";
+int buttonState = 0;     // variable for reading the pushbutton status
+
+int numSteps = 0;
+int numFunction = 0;
+
+boolean functionFound = false;
+String functionType = "function";
+
 void setup() {  
 
   // initialize led pins to output
@@ -70,99 +80,162 @@ void setup() {
   Serial.begin(9600);
   shifter.clear(); //set all pins on the shift register chain to LOW  
   delay(1000);  
+
+  Serial.println("Starting...");
 }
 
 void loop() {
 
-  if (charPresent == true)  // check if character game piece on board
+  int reading = analogRead(charPin); 
+  //    Serial.println(reading);
+
+  int minReading = charVal - range;
+  int maxReading = charVal + range;
+
+  if (reading > maxReading || reading < minReading)
   {
-    // read the state of the pushbutton value:
-    buttonState = digitalRead(buttonPin);    
+    if (reading < 1000) 
+    {
+      Serial.print("show character ");   
+      type = checkCharType(reading, type);        
+      Serial.println(type);     
+    } 
+    else {
+      Serial.print("hide character ");   
+      type = checkCharType(charVal, type);     
+      Serial.println(type);   
+    }
+    charVal = reading; 
+    delay(50);
+  }
 
-    // Button pressed.
-    if (buttonState == LOW) 
-    {     
-      // turn status LED on    
-      digitalWrite(statusLED, HIGH);  
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);    
 
-      // debounce button input for 50ms
-      int x;
-      while(x < 50)
-      {
-        if (digitalRead(buttonState) == HIGH)
-          x++;
-        else
-          x = 0;
-        delay(1);
-      } 
+  // Button pressed.
+  if (buttonState == LOW && charVal < 1000) 
+  {     
+    // turn status LED on    
+    digitalWrite(statusLED, HIGH);  
 
-      shifter.clear(); //set all pins on the shift register to LOW   
-      shifter.write();   
-
-      Serial.print("Steps values: ");
-      for (int pin=0; pin < stepsPins; pin++)
-      {
-        Serial.print(stepsValues[pin]);      
-        Serial.print(" ");      
-      }
-      Serial.println();
-
-      Serial.print("Function values: ");
-      for (int pin=0; pin < functionPins; pin++)
-      {
-        Serial.print(functionValues[pin]);      
-        Serial.print(" ");      
-      }
-      Serial.println();    
-
-      //turn off function LEDs
-      for (int ledPin=6; ledPin<10; ledPin++)
-      {
-        digitalWrite(ledPin, LOW);
-      }
-
-      // Blink status led
-      for (int i=0; i<5; i++)
-      {
-        digitalWrite(statusLED, HIGH);
-        delay(50);
-        digitalWrite(statusLED, LOW);
-        delay(50);
-      }      
+    // debounce button input for 50ms
+    int x;
+    while(x < 50)
+    {
+      if (digitalRead(buttonState) == HIGH)
+        x++;
+      else
+        x = 0;
+      delay(1);
     } 
 
-    // Button not pressed. Read game block values from STEPS and FUNCTION rows
-    else 
-    {
-      // turn LED off:
-      digitalWrite(statusLED, LOW);    
+    //set all steps pins on the shift register to LOW
+    shifter.clear();   
+    shifter.write();   
 
-      // read the values on each pin and save it to their arrays
-      for (int pin=0; pin < stepsPins; pin++)
-      {
-        amux.SelectPin(pin); // choose the pin you want to send signal to off the DeMux
-        uint16_t reading = amux.AnalogRead();    
-        stepsValues[pin] = reading; 
-
-        //      Serial.print(reading);      
-        //      Serial.println(" ");
-
-        if (reading < 1000) // check if block present
-        {
-          shifter.setPin(pin, HIGH);  // turn respective led on if block is present
-        }
-        else
-        {
-          shifter.setPin(pin, LOW);  // turn respective led off if block not present
-        }
-        shifter.write();     
+    // count how many sequential step blocks are present; ignore any  after an empty space
+    for (int pin = 0; pin < stepsPins; pin++) {
+      int value = stepsValues[pin];
+      if (value > 1000) {
+        break;
+      } 
+      else {
+        numSteps++;
       }
+    }   
 
+    // count how many sequential step blocks are present; ignore any  after an empty space   
+    for (int pin = 0; pin < functionPins; pin++) {
+      int value = functionValues[pin];
+      if (value > 1000) {
+        break;
+      } 
+      else {
+        numFunction++;
+      }
+    }         
+
+    // print step values
+    Serial.println(numSteps);
+    for (int pin=0; pin < numSteps; pin++)
+    {
+      int value = stepsValues[pin];
+      type = checkType(value, type);    
+      if (type.equals(functionType))
+      {
+        functionFound = true;
+      }        
+      Serial.println(type);    
+    }
+
+    // print function values
+
+    if (functionFound == true) 
+    {
+      Serial.println(numFunction);
+      for (int pin=0; pin < numFunction; pin++)
+      {
+        int value = functionValues[pin];
+        type = checkType(value, type);
+        Serial.println(type);   
+      }
+    } 
+    else {
+      Serial.println("0");
+    }
+
+    //turn off function LEDs
+    for (int ledPin=6; ledPin<10; ledPin++)
+    {
+      digitalWrite(ledPin, LOW);
+    }
+
+    // reset all values to 0 after data sent
+    numSteps = 0;
+    numFunction = 0;    
+    functionFound = false;
+    delay(1000);      
+
+    Serial.println("******************");
+  }
+
+  // Button not pressed. Read game block values from STEPS and FUNCTION rows
+  else 
+  {
+    // turn LED off:
+    digitalWrite(statusLED, LOW);    
+
+    // read the values on each pin and save it to their arrays
+    for (int pin=0; pin < stepsPins; pin++)
+    {
+      amux.SelectPin(pin); // choose the pin you want to send signal to off the DeMux
+      uint16_t reading = amux.AnalogRead();    
+      stepsValues[pin] = reading; 
+
+      //   print steps readings
+      //        Serial.println("steps");      
+      //        Serial.println(reading);
+
+      if (reading < 1000) // check if block present
+      {
+        shifter.setPin(pin, HIGH);  // turn respective led on if block is present
+      }
+      else
+      {
+        shifter.setPin(pin, LOW);  // turn respective led off if block not present
+      }
+      shifter.write();     
+
+      // turn on respective function led if block present
       for (int pin=0; pin < functionPins; pin++)
       {
         int reading = analogRead(pin);
         int ledPin = pin+6;
         functionValues[pin] = reading;       
+
+        // print function readings
+        //        Serial.println("Function: ");
+        //        Serial.println(reading);
 
         if (reading < 1000)    
         {  
@@ -173,20 +246,60 @@ void loop() {
           digitalWrite(ledPin,LOW);
         }  
       } 
-    }
-  }    
-  else // character not on board; keeps checking for reading
-  {
-    int charValue = analogRead(charPin);
-    if (charValue < 550)
-    {
-      Serial.print("Character: ");
-      Serial.println(charValue);
-      charPresent = true;  
-    }
+    }    
   }
+
 }
 
+String checkType(int value, String type) {
+  if (value > 0 && value < 6) {
+    type = "up";  
+  } 
+  else if (value > 6 && value < 12) {
+    type = "down";  
+  } 
+  else if (value > 15 && value < 25) {
+    type = "left";
+  } 
+  else if(value > 25 && value < 35) { 
+    type = "right"; 
+  } 
+  else if (value > 48 && value < 60) {
+    type = "dance";
+  } 
+  else if (value > 85 && value < 98) {
+    type = "magic";
+  } 
+  else if (value > 175 && value < 185) {
+    type = "tantrum";  
+  } 
+  else if (value > 245 && value < 260) {
+    type = "dress-up";
+  }  
+  else if (value > 320 && value < 330) {
+    type = "sleep";
+  } 
+  else if (value > 505 && value < 515) {
+    type = "function";
+  }  
+  else if (value > 675 && value < 690) {
+    type = "love";
+  }
+  else if (value > 1000)  { 
+    type = "";
+  }
+  return type;
+}
 
-
-
+String checkCharType(int value, String type) {
+  if (value > 0 && value < 10) {
+    type = "pink";  
+  } 
+  else if(value > 25 && value < 35) { 
+    type = "blue"; 
+  } 
+  else if (value > 505 && value < 515) {
+    type = "green";
+  }
+  return type;
+}
